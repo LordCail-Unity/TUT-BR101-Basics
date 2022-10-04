914da0ecf894434fd001f46ba180441acb06fac6
LordCail-Unity <114961576+LordCail-Unity@users.noreply.github.com>
2022-10-04 11:03:19 +1100

    Imported Project
    Project Files start at Brackeys How to Make a Game Basics Tutorial E06
    
    https://www.youtube.com/watch?v=D4I0I3QJAvc&list=PLPV2KyIb3jR53Jce9hP7G5xC4O9AgnOuL

---
+++ TUT-BR101-Basics/Assets/0/Scripts/CameraMovement.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public Transform player;
+    public Vector3 offset;
+
+    private void Update()
+    {
+        transform.position = player.position + offset;
+    }
+
+}
---
+++ TUT-BR101-Basics/Assets/0/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody _rigidBody;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+
+    // Unity prefers FixedUpdate() method instead of Update() for physics 
+    void FixedUpdate()
+    {
+        
+        // Add a forward force
+        _rigidBody.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if ( Input.GetKey("d"))
+        {
+            _rigidBody.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            _rigidBody.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+    }
+
+}
