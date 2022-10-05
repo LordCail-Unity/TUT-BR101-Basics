6b136ec0669ce8d27d36862f15f8caecb014c813
LordCail-Unity <114961576+LordCail-Unity@users.noreply.github.com>
2022-10-05 17:30:40 +1100

    Brackeys E11 Loading Bar
    =================================
    PROJECT UPDATE NOTES: E10 EXTENDED
    =================================
    
    == Build Project ==
    
    Link:
    > https://www.youtube.com/watch?v=7nxKAtxGSn8
    
    _ BUT we want to update the Level Manager first
    _ AND we want to change the input system to work with touch screen (Horizontal Axis NOT specific key press)
    
    == Level Manager ==
    
    _ It's hard to manage various aspects of UI etc across multiple levels so I want to introduce a proper Scene Manager
    _ Found a Brackeys tutorial that covers this off better than the Tarodev one so will attempt to insert into this project AFTER E10
    > https://www.youtube.com/watch?v=YMj2qPq9CP8
    
    _ Simply replaced the MenuManager script contents with Brackeys LevelLoader script on the Start button and it works.
    _ Explanation of why you need to use a weighted value against 0.9 as the loading percentage:
    > https://youtu.be/YMj2qPq9CP8?t=315
    
    Updated sections for TextMeshPro (using TMPro;) and changed the progress bar to hang at 90% rather than hang at 100%.

--- TUT-BR101-Basics/Assets/0/Scripts/LevelManager.cs
+++ TUT-BR101-Basics/Assets/0/Scripts/LevelManager.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 public class LevelManager : MonoBehaviour
 {
 
+    
     public void LoadNextLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
